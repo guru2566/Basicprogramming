809115d5f6c93d8e18c192bf82080ee6ec0355ee
guru2566 <36722072+guru2566@users.noreply.github.com>
2018-08-11 19:12:54 +0530

    Create addfun.c

---
+++ addfun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+  int x;
+  x=a+b;
+  return x;
+  
+}
+int main()
+{
+  int a,b,x;
+  scanf("%d\n%d",&a,&b);
+  x=add(a,b);
+  printf("%d + %d = %d",a,b,c);
+  return 0;
+ }
